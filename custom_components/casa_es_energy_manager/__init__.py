99507571262ef100ae5bb0e0759252fdb97b6312
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-08-25 21:44:01 +0200

    v1.0.0: use v1 coordinator

--- custom_components/casa_es_energy_manager/__init__.py
+++ custom_components/casa_es_energy_manager/__init__.py
@@ -9,7 +9,7 @@ from homeassistant.helpers.event import async_call_later, async_track_time_inter
 
 from .ai_planner import CasaESAIPlanner
 from .const import DOMAIN
-from .coordinator import CasaESEnergyCoordinator
+from .coordinator_v1 import CasaESEnergyCoordinator
 
 PLATFORMS: list[Platform] = [
     Platform.SENSOR,
